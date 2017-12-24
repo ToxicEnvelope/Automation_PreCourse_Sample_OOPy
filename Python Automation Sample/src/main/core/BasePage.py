bc15e0c7824f8ad43e182c241e00c462dddc5846
Yahav N. Hoffman <ynhoffman@gmail.com>
2017-12-24 15:26:34 +0200

    extending basecode abilities

--- Python Automation Sample/src/main/core/BasePage.py
+++ Python Automation Sample/src/main/core/BasePage.py
@@ -22,51 +22,78 @@ except ImportError as e:
 # we can add on the tests section ClassName(unittest.TestCase) for testing implementations
 class BasePage:
 
+	# CONSTRUCTOR
 	def __init__(self, driver):
 		this._driver = driver
 
+	# CLICK on WebElement
 	def click(self, element):
 		try:
 			self._driver.execute_script("argumets[0].setAttribute('sytle', 'borde: 2px solid green');")
 			element.click()
+			wait(1)
 		except wde as e:
 			print str(e)
 			self._driver.execute_script("argumets[0].setAttribute('sytle', 'borde: 2px solid green');")
 			self._driver.execute_script("argumets[0].click();")
+			wait(1)
 
-
+	# FILL_TEXT to WebElement given String 
 	def fill_text(self, element, word):
 		try:
 			self._driver.execute_script("argumets[0].setAttribute('sytle', 'borde: 2px solid blue');")
 			element.send_keys(word)	
+			wait(1)
 		except wde as e:
 			self._driver.execute_script("argumets[0].setAttribute('sytle', 'borde: 2px solid blue');")
 			self._driver.execute_script("argumets[0].vaule = " + word)
+			wait(1)
 
+	# TAKE SCREEN SHOTS
+	def snap():
+		try:
+			self._driver.get_screenshot_as_file(get_timestamp() + ".png")
+		except Exception as e:
+			print str(e)
+
+	# WAIT 'x' seconds
 	def wait(self, sec):
 		try:
 			time.sleep(sec)
 		except Exception as e:
 			print str(e)
 
+	# WAIT for WebElement until is visible in DOM given cssSelector
 	def wait_until_visible_by_CSS(self, css):
 		try:
 			return WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located((By.selector(css))))
 		except wde as e:
 			print str(e)
 
+	# WAIT for WebElement until is present in DOM given id
 	def wait_until_visible_by_ID(self, id):
 		try:
 			return WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located((By.id(id)))))	
 		except wde as e:
 			print str(e)
+
+	# WAIT for WebElement until is visible in DOM given cssSelector
 	def wait_until_presents_by_CSS(self, css):
 		try:
 			return WebDriverWait(self._driver, 10).until(EC.presence_of_element_located((By.selector(css))))
 		except wde as e:
 			print str(e)
+
+	# WAIT for WebElement until is present in DOM given id
 	def wait_until_presents_by_ID(self, id):
 		try:
 			return WebDriverWait(self._driver, 10).until(EC.presence_of_element_located((By.id(id))))
 		except wde as e:
+			print str(e)
+
+	# PRIVATE USE
+	def get_timestamp():
+		try:
+			return time.asctime()
+		except Exception as e:
 			print str(e)
--- Python Automation Sample/src/main/core/pageobjects/FBPage.py
+++ Python Automation Sample/src/main/core/pageobjects/FBPage.py
@@ -7,18 +7,31 @@ class FBPage(BasePage, obect):
 	def init(self):
 		BasePage.__init__(self)
 
-	def attempt_to_authenticate(self, user, pass):
+	def attempt_to_authenticate(self, user, passwd):
 		try:	
 			user_field = super(BasePage, self).wait_until_visible_by_CSS("input#email.inputtext")
 			if user_field.is_displayed():
 				super(BasePage, self).fill_text(user)
+				pass
+			else:
+				super(BasePage, self).snap()
+				break
 			pass_field = super(BasePage, self).wait_until_visible_by_CSS("input#pass.inputtext")
 			if pass_field.is_displayed():
-				super(BasePage, self).fill_text(pass)
+				super(BasePage, self).fill_text(passwd)
+				pass
+			else:
+				super(BasePage, self).snap()
+				break
 			login_btn = super(BasePage, self).wait_until_visible_by_CSS("label > input#loginbutton")
 			if login_btn.is_displayed():
 				super(BasePage, self).click(login_btn)
+				pass
+			else:
+				super(BasePage, self).snap()
+				break
 			return True
 		except Exception as e:
 			print str(e)
 			return False
+
